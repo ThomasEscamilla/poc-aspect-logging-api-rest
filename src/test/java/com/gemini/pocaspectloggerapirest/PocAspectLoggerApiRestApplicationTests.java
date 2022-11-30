1373fa81648eb593acfebfeeeaf5963af35eff6b
ESCAMILLA <thomas.escamilla@capgemini.com>
2022-11-30 10:40:49 +0100

    Initial commit

---
+++ src/test/java/com/gemini/pocaspectloggerapirest/PocAspectLoggerApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gemini.pocaspectloggerapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocAspectLoggerApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
